5174cb41759514fadeabb5adcdb598f575e1b37e
Keshav Soni <keshavvsonii01@gmail.com>
2025-04-13 15:41:37 +0530

    Update About

--- components/pages/about.jsx
+++ components/pages/about.jsx
@@ -1,11 +1,11 @@
 import TrueFocus from "../ui/TrueFocus";
-import Image from "next/image";
-import SplitText from "../ui/SplitText";
+import TiltedCard from "../ui/TiltedCard";
+import BlurText from "../ui/BlurText";
 
 const About = () => {
   return (
     <>
-      <div className="p-6 m-4 h-screen flex flex-col items-center justify-center">
+      <div className="p-6 mt-4 h-screen flex flex-col items-center justify-center">
         <TrueFocus
           sentence="About Me"
           manualMode={false}
@@ -15,24 +15,30 @@ const About = () => {
           pauseBetweenAnimations={0.8}
           glowColor="rgba(209,202,222, 0.6)"
         />
-        <Image
-          src={"/images/aboutme.jpg"}
-          alt="About Me"
-          width={800}
-          height={600}
-          className="rounded-2xl mx-auto my-6"
-        />
+        <div className="p-2 text-center my-8">
+          <TiltedCard
+            imageSrc="/images/aboutme.jpg"
+            altText="Keshav Soni"
+            captionText="Keshav Soni"
+            containerHeight="400px"
+            containerWidth="800px"
+            imageHeight="400px"
+            imageWidth="800px"
+            rotateAmplitude={10}
+            scaleOnHover={1.05}
+            showMobileWarning={false}
+            showTooltip={true}
+            displayOverlayContent={true}
+            overlayContent={<p className="tilted-card-demo-text"></p>}
+          />
+        </div>
         <div className="text-center mt-4">
-          <p className="text-xl font-light"></p>
-          <SplitText
-            text=" Empowering all-scale startups with custom web experiences that are memorable and drive success. With a passion for design and development, I take projects from ideation to launch, ensuring a seamless journey that leaves a lasting impact in the digital landscape."
-            className="text-2xl font-semibold text-center"
-            delay={15}
-            animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
-            animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-            easing="easeOutCubic"
-            threshold={0.2}
-            rootMargin="-50px"
+          <BlurText
+            text="Empowering all-scale startups with custom web experiences that are memorable and drive success. With a passion for design and development, I take projects from ideation to launch, ensuring a seamless journey that leaves a lasting impact in the digital landscape."
+            delay={50}
+            animateBy="words"
+            direction="top"
+            className="text-2xl my-4 line-clamp-3"
           />
         </div>
       </div>
