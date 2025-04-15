7ed622c37dd2d4262cd96d8cbb62b391cbcc65e1
Keshav Soni <keshavvsonii01@gmail.com>
2025-04-16 01:20:50 +0530

    Testimonial Scratch

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import About from "@/components/pages/about";
 import FastScroller from "@/components/pages/fast-scroller";
 import Hero from "@/components/pages/hero";
 import Projects from "@/components/pages/projects";
+import Testimonials from "@/components/pages/testimonials";
 import Whatido from "@/components/pages/whatido";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <About />
         <Whatido />
         <Projects />
+        <Testimonials />
     </>
   );
 }
---
+++ components/pages/testimonials.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Testimonials = () => {
+    return (
+        <>
+            <h1>Hey</h1>        
+        </>
+    )
+}
+
+export default Testimonials;
