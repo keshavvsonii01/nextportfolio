3bbe6f27a6f4038573f8f88727a8914b68b8fa14
Keshav Soni <keshavvsonii01@gmail.com>
2025-04-21 15:44:04 +0530

    Testimonial Scratch

--- components/pages/projects.jsx
+++ components/pages/projects.jsx
@@ -33,47 +33,48 @@ const Projects = () => {
 
   return (
     <>
-      <div className="h-screen bg-black text-white  p-8 mx-4  shadow-xl rounded-lg">
-        <div className="flex flex-col items-center w-full my-2">
-          <SplitText
-            text="PROJECTS."
-            className="text-8xl font-extrabold text-center p-2 m-2"
-            delay={200}
-            animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
-            animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-            easing="easeOutCubic"
-            threshold={0.2}
-            rootMargin="-50px"
-          />
-        </div>
-        {/* <div style={{ height: "100%", position: "relative" }}>
+      <div className="h-svh">
+        <div className="h-screen bg-black text-white  p-8 mx-4  shadow-xl rounded-lg">
+          <div className="flex flex-col items-center w-full my-2">
+            <SplitText
+              text="PROJECTS."
+              className="text-8xl font-extrabold text-center p-2 m-2"
+              delay={200}
+              animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+              animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+              easing="easeOutCubic"
+              threshold={0.2}
+              rootMargin="-50px"
+            />
+          </div>
+          {/* <div style={{ height: "100%", position: "relative" }}>
           <InfiniteMenu className="text-xl" items={items} />
         </div> */}
-        <div className="flex p-4 mx-6 shadow-2xl rounded-xl">
-          <div className="flex flex-2/4 w-3/4 justify-around p-4 rounded-2xl">
-            <MagicCard>
-              <div className="p-4 flex flex-col items-center justify-center">
-                <Image
-                  src="/images/pic14.png"
-                  alt="pic14"
-                  width={400}
-                  height={500}
-                  className="rounded-lg w-fit"
-                />
-                <div className="flex flex-col items-center justify-center text-center my-6">
-                  <p className="m-4 text-2xl font-bold">IDEA GENIE</p>
-                  <span className="text-lg italic ">
-                    An AI-powered web app that generates ideas for you.
-                  </span>
-                </div>
+          <div className="flex p-2 mx-6 shadow-2xl rounded-xl">
+            <div className="flex flex-2/4 w-3/4 justify-around p-4 rounded-2xl">
+              <MagicCard>
+                <div className="p-4 flex flex-col items-center justify-center">
+                  <Image
+                    src="/images/pic14.png"
+                    alt="pic14"
+                    width={400}
+                    height={500}
+                    className="rounded-lg w-fit"
+                  />
+                  <div className="flex flex-col items-center justify-center text-center my-6">
+                    <p className="m-4 text-2xl font-bold">IDEA GENIE</p>
+                    <span className="text-lg italic ">
+                      An AI-powered web app that generates ideas for you.
+                    </span>
+                  </div>
 
-                <ShimmerButton>
-                  <a href="https://ideagenie.netlify.app/" target="_blank">
-                    View!
-                  </a>
-                </ShimmerButton>
-              </div>
-            </MagicCard>
+                  <ShimmerButton>
+                    <a href="https://ideagenie.netlify.app/" target="_blank">
+                      View!
+                    </a>
+                  </ShimmerButton>
+                </div>
+              </MagicCard>
             </div>
             <div className="flex flex-2/4 w-3/4 justify-around p-4 rounded-2xl">
               <MagicCard>
@@ -102,30 +103,31 @@ const Projects = () => {
               </MagicCard>
             </div>
             <div className="flex flex-2/4 w-3/4 justify-around p-4 rounded-2xl">
-            <MagicCard>
-              <div className="p-4 flex flex-col items-center justify-center">
-                <Image
-                  src="/images/pic12.png"
-                  alt="pic14"
-                  width={400}
-                  height={500}
-                  className="rounded-lg w-fit"
-                />
-                <div className="flex flex-col items-center justify-center text-center my-6">
-                  <p className="m-4 text-2xl font-bold">KREATIVEVILLA</p>
-                  <span className="text-lg italic ">
-                  A Social Media Marketing Agency.
-                  </span>
-                </div>
+              <MagicCard>
+                <div className="p-4 flex flex-col items-center justify-center">
+                  <Image
+                    src="/images/pic12.png"
+                    alt="pic14"
+                    width={400}
+                    height={500}
+                    className="rounded-lg w-fit"
+                  />
+                  <div className="flex flex-col items-center justify-center text-center my-6">
+                    <p className="m-4 text-2xl font-bold">KREATIVEVILLA</p>
+                    <span className="text-lg italic ">
+                      A Social Media Marketing Agency.
+                    </span>
+                  </div>
 
-                <ShimmerButton>
-                  {" "}
-                  <a href="https://aiessaypro.netlify.app/" target="_blank">
-                    View!
-                  </a>
-                </ShimmerButton>
-              </div>
-            </MagicCard>
+                  <ShimmerButton>
+                    {" "}
+                    <a href="https://aiessaypro.netlify.app/" target="_blank">
+                      View!
+                    </a>
+                  </ShimmerButton>
+                </div>
+              </MagicCard>
+            </div>
           </div>
         </div>
       </div>
--- components/pages/testimonials.jsx
+++ components/pages/testimonials.jsx
@@ -1,12 +1,24 @@
-
-
+import SplitText from "../ui/SplitText";
 
 const Testimonials = () => {
-    return (
-        <>
-            <h1>Hey</h1>        
-        </>
-    )
-}
+  return (
+    <>
+      <div className="h-svh mt-6 flex justify-center shadow-xl rounded-lg">
+        <div>
+          <SplitText
+            text="DON'T TAKE MY WORD FOR IT."
+            className="text-[5.5rem] font-extrabold text-center m-2"
+            delay={75}
+            animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+            animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+            easing="easeOutCubic"
+            threshold={0.2}
+            rootMargin="-50px"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Testimonials;
