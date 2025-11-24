37473836c057a0abc0ede18d51670d651ee7df86
Keshav Soni <keshavvsonii01@gmail.com>
2025-11-24 21:46:33 +0530

    change

--- app/contact-temp/page.jsx
+++ app/contact-temp/page.jsx
@@ -3,6 +3,7 @@ import { LoginForm } from "@/components/login-form";
 function Contact() {
   return (
     <>
+
       <div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="w-full max-w-sm">
           <LoginForm />
